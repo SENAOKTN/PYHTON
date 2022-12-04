ebba0a48858cf546e6ac289376c98856c39caefb
SENAOKTN <119498087+SENAOKTN@users.noreply.github.com>
2022-12-04 19:21:07 +0300

    Add files via upload

---
+++ kulanıcıdan_alınan_üç_sayının_çarpımı.py
@@ -0,0 +1,5 @@
+birinci_sayı=int(input("birinci sayıyı giriniz:"))
+ikinci_sayı=int(input ("ikinci sayıyı giriniz:"))
+üçüncü_sayı=int(input("üçüncü sayıyı giriniz:"))
+çarpım=birinci_sayı*ikinci_sayı*üçüncü_sayı
+print(çarpım)
