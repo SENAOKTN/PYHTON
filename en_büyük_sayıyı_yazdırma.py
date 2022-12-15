2d56eabacc461ee4e2dc9e252e9463e73c2b916d
SENAOKTN <119498087+SENAOKTN@users.noreply.github.com>
2022-12-15 10:10:15 +0300

    Add files via upload

---
+++ en_büyük_sayıyı_yazdırma.py
@@ -0,0 +1,14 @@
+a =  int(input("a:"))
+
+b = int(input("b:"))
+
+c = int(input("c:"))
+
+if(a>=b and a>=c):
+    print("A EN BÜYÜK SAYI")
+
+elif(b>=a and b>=c ):
+    print("B EN BÜYÜK SAYI")
+
+else:
+    print("c en büyük sayıs")
